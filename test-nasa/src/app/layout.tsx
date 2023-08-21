dcc81882c32acd2f48089194ca0a90781967d28d
AlinaLaniuk <alinavoronina16@gmail.com>
2023-08-21 15:54:18 +0300

    feat: add background

--- test-nasa/src/app/layout.tsx
+++ test-nasa/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export const metadata: Metadata = {
   title: 'Asteroids App',
@@ -13,7 +15,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className={styles.appBackground}>
+          <div className={styles.planetaImgContainer}>
+            <Image
+              src={'/planeta.png'}
+              width={0}
+              height={0}
+              sizes='100vw'
+              style={{ width: 'auto', height: '100%' }}
+              alt='planeta-img' 
+            />
+          </div>
+          {children}
+        </div>
+        </body>
     </html>
   )
 };
